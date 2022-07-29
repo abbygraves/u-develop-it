9fd84a25494ebc87d8166480a4f0538d1c22a8e6
abbygraves <abbygraves14@gmail.com>
2022-07-29 05:00:15 -0500

    set up express.js and server.js file

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// EXPRESS MIDDLEWARE
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+
+
+// ROUTES
+// Default response for any other request (not found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
